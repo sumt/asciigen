d3397eeb07dac5396f45ad2c4e91ee8217501317
Armand <adarcorerlreth@gmail.com>
2012-05-06 21:42:38 -0400

    added website html; css; added jinja2 and pil support config; added image conversion class without implementation

--- main.py
+++ main.py
@@ -15,11 +15,55 @@
 # limitations under the License.
 #
 import webapp2, cgi, re
+from jinja2 import Template
+
+#ASCIIGEN Prototype
+
+asciigen_template = Template("""
+<html>
+<head>
+<link type="text/css" rel="stylesheet" href="{{ cssfile }}">
+<title>{{ page_title }}</title>
+</head>
+<body>
+<a href="{{ selfpage }}"><h2>/ASCIIGEN/</h2></a>
+<div>
+<form method="post">
+  <label>
+    <div>Image File</div>
+    <input type="file" name="image">
+  </label>
+  <input type="submit" value="Upload">
+</form>
+</div>
+</body>
+</html>
+""")
+
+asciigen_title = "ASCII"
+asciigen_page = "/asciigen"
+
+class AsciiImageGenerator():
+  def imageToAscii():
+    pass
+
+class GeneralHandler(webapp2.RequestHandler):
+  def write(self, template, **args):
+    self.response.out.write(template.render(args))
+
+class AsciiGenHandler(GeneralHandler):
+  def get(self):
+    cssfile = "stylesheet/asciigen.css"
+    self.write(asciigen_template, cssfile = cssfile, page_title = asciigen_title, selfpage = asciigen_page)
+
+  def post(self):
+    pass
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
       self.response.headers['Content-Type'] = 'text/plain'
       self.response.out.write("ASCIIGEN")
 
-app = webapp2.WSGIApplication([('/', MainHandler)],
+app = webapp2.WSGIApplication([('/', MainHandler),
+                               (asciigen_page, AsciiGenHandler)],
                               debug=True)
