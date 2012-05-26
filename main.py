b871589980d6527eba90d4fb28b54bde7242d680
Armand <adarcorerlreth@gmail.com>
2012-05-26 11:08:41 -0400

    Implemented capability of uploading images and display the upload image

--- main.py
+++ main.py
@@ -16,6 +16,9 @@
 #
 import webapp2, cgi, re
 from jinja2 import Template
+from google.appengine.ext import blobstore
+from google.appengine.ext import db
+from google.appengine.api import images
 
 #ASCIIGEN Prototype
 
@@ -28,10 +31,10 @@ asciigen_template = Template("""
 <body>
 <a href="{{ selfpage }}"><h2>/ASCIIGEN/</h2></a>
 <div>
-<form method="post">
+<form method="post" enctype="multipart/form-data" action="{{ imageHandlerPage }}">
   <label>
     <div>Image File</div>
-    <input type="file" name="image">
+    <input type="file" name="{{ vImageName }}" size="40">
   </label>
   <input type="submit" value="Upload">
 </form>
@@ -41,29 +44,68 @@ asciigen_template = Template("""
 """)
 
 asciigen_title = "ASCII"
-asciigen_page = "/asciigen"
 
+#global website addresses
+asciigen_page = "/asciigen"
+image_debug_page = "/debug/([^/]+)?"
+image_debug_page_sub = "/debug/%(blobid)s"
+upload_url = "/upload"
+
+#global webpage variable
+image_name = "image"
+
+#global css file for all sites
+cssfile = "stylesheet/asciigen.css"
+
+#database containing images that we'll save temporarily
+class ImageDB(db.Model):
+  mImage = blobstore.BlobReferenceProperty(required = True) #blobInfo of the raw image file
+
+#extends this to handle image trnsformation into ASCII
 class AsciiImageGenerator():
   def imageToAscii():
+    #TODO: interface class
     pass
 
+#Superclass that defines useful methods for all website handlers
 class GeneralHandler(webapp2.RequestHandler):
   def write(self, template, **args):
     self.response.out.write(template.render(args))
 
 class AsciiGenHandler(GeneralHandler):
   def get(self):
-    cssfile = "stylesheet/asciigen.css"
-    self.write(asciigen_template, cssfile = cssfile, page_title = asciigen_title, selfpage = asciigen_page)
+    self.write(asciigen_template, cssfile = cssfile, page_title = asciigen_title, selfpage = asciigen_page, imageHandlerPage = blobstore.create_upload_url(upload_url), vImageName = image_name)
 
   def post(self):
-    pass
+    blob_key = blobstore.parse_blob_info(self.request.POST[image_name])
+    img = ImageDB(mImage = blob_key)
+    img.put()
+    #redirect to the debug page that shows the image
+    self.redirect(image_debug_page_sub % {"blobid" : img.key().id()}) #TODO: replace 
+
+#DEBUG Handler that shows the image being uploaded
+class ShowImageHandler(webapp2.RequestHandler):
+  def get(self, image_id):
+    if image_id.isdigit():
+      img_file = ImageDB.get_by_id(int(image_id))
+      if img_file:
+        blob_reader = blobstore.BlobReader(img_file.mImage)
+        raw_file = blob_reader.read() #get the raw data of the image from blobInfo key
+        img = images.Image(raw_file)
+        img.im_feeling_lucky() #a random transformation, 1 transformation is required
+        thumbnail = img.execute_transforms(output_encoding = images.JPEG)
+        self.response.headers['Content-Type'] = 'image/jpeg'
+        self.response.out.write(thumbnail)
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
       self.response.headers['Content-Type'] = 'text/plain'
       self.response.out.write("ASCIIGEN")
 
+#define a list of websites our server will handle, and map them to the handler class
 app = webapp2.WSGIApplication([('/', MainHandler),
-                               (asciigen_page, AsciiGenHandler)],
+                               (asciigen_page, AsciiGenHandler),
+                               (upload_url, AsciiGenHandler),
+                               (image_debug_page, ShowImageHandler)],
                               debug=True)
+
