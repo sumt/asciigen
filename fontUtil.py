68fda2a059367621e6ed7284bf79c9a36f08902d
Armand <adarcorerlreth@gmail.com>
2012-06-24 21:39:29 -0400

    added util class functions
    fontImageSize - retrieve the image size of a letter in CourierNew font
    with specified font size as parameter
    squareDist - calculate the square distance between two image treated as
    vectors

--- fontUtil.py
+++ fontUtil.py
@@ -1,5 +1,12 @@
 from PIL import Image, ImageDraw, ImageFont
 
+#return the image size of one letter based on font size
+def fontImageSize(font_size):
+  font = ImageFont.truetype("CourierNew.ttf", font_size)
+  return font.getsize("A")
+
+#create a PIL.Image object containing the character string in parameter letter
+#return: PIL.Image of the string of letters in black with white background
 def getCharImage(letter, font_size):
   font = ImageFont.truetype("CourierNew.ttf", font_size)
   image_size = font.getsize(letter)
@@ -7,3 +14,12 @@ def getCharImage(letter, font_size):
   draw = ImageDraw.Draw(image)
   draw = draw.text((0,0), letter, 0, font=font)
   return image
+
+#calculate the square distance between 2 images as if they are two vectors
+#with each pixel representing a dimention
+def squareDist(image1, image2):
+  image1_data = list(image1.getdata())
+  image2_data = list(image2.getdata())
+  minlen = min(len(image1_data), len(image2_data))
+  square_dist = reduce(lambda x, y: x + (y[1] - y[0])**2, zip(image1_data, image2_data), 0)
+  return square_dist
